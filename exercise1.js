77b31847d236473f5cdec7ebd0111b98d0cbf073
DerJontte <john.nordqvist@gmail-com>
2018-11-30 15:21:58 +0200

    E1 logic ready

--- exercise1.js
+++ exercise1.js
@@ -10,19 +10,61 @@ function errorMessage(message) {
 }
 
 function parseDate(rawDate) {
-
+    if (rawDate == 0) {
+        var date = "00.00.00";
+    } else {
+        year = "20" + rawDate.slice(0, 2);
+        month = parseInt(rawDate.slice(2, 4), 10);
+        day = parseInt(rawDate.slice(-2), 10);
+        var date = day + "." + month + "." + year;
+    }
+    return date;
 }
 
-var parseVirtual = function(){
+function formatIBAN(rawNo) {
+    formatted = rawNo.slice(0,2) + " " + rawNo.slice(2, 6) + " " + rawNo.slice(6, 10) + " " + rawNo.slice(10, 14) + " " + rawNo.slice(-2);
+    return formatted;
+}
+
+function refVer4(rawRefNo){
+    rawRefNo = rawRefNo.replace(/^0+/, "");
+    refNo = "";
+    while (rawRefNo.length > 4) {
+        refNo = rawRefNo.slice(-5) + " " + refNo;
+        rawRefNo = rawRefNo.slice(0, -5);
+    }
+    refNo = rawRefNo + " " + refNo;
+    return refNo;
+}
+
+function refVer5(rawRefNo) {
+    refNo = "RF" + rawRefNo.slice(0,2);
+    rawRefNo = rawRefNo.slice(2).replace(/^0+/, "");
+    while (rawRefNo.length > 3) {
+        refNo += " " + rawRefNo.slice(0, 4);
+        rawRefNo = rawRefNo.slice(4);
+    }
+    refNo += " " + rawRefNo;
+    return refNo;
+}
+
+function parseVirtual() {
     vCode = document.getElementById("virtual_barcode").value;
     if (vCode.length != 54){
         return errorMessage("invalid barcode length");
     }
 
     version = vCode.slice(0, 1);
-    iban = vCode.slice(1, 17);
+    if (version == 4) {
+        refNo = refVer4(vCode.slice(28, 48));
+    } else if (version == 5) {
+        refNo = refVer5(vCode.slice(25, 48));
+    } else {
+        errorMessage("invalid barcode version");
+        return;
+    }
+    iban = formatIBAN(vCode.slice(1, 17));
     amount = parseInt(vCode.slice(17, 23), 10) + "," + vCode.slice(23, 25);
-    refNo = vCode.slice(25, 48);
     dueDate = parseDate(vCode.slice(-6));
 
     document.getElementById("iban").innerText = iban;
@@ -30,10 +72,23 @@ var parseVirtual = function(){
     document.getElementById("reference").innerText = refNo;
     document.getElementById("due_date").innerText = dueDate;
 
-    document.getElementById('barcode').innerText = vCode;
+    var element = document.getElementById("barcode");
+    JsBarcode(element, vCode, {format: "CODE128C"});
+}
+
+function setGreyOnFocus(element) {
+    element.onfocus = function() {
+            element.style = "background: lightgrey";
+        }
+    element.onblur = function() {
+            element.style = "background: white";
+        }
 }
 
 window.onload = function(n) {
-    //Attach the onclick handler defined in step 1
+    textInputs = document.getElementsByClassName("text_input");
+    for (i = 0; i < textInputs.length; i++) {
+        setGreyOnFocus(textInputs[i]);
+    }
     document.getElementById("btn_decode").onclick = parseVirtual;
 }
