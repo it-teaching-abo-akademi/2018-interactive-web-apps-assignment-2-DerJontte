b9800f2cc5bdbf7c3eaa09f27e2f3382847a028b
DerJontte <john.nordqvist@gmail-com>
2018-12-02 23:44:23 +0200

    Submission

--- excercise1/exercise1.js
+++ excercise1/exercise1.js
@@ -1,7 +1,70 @@
+// A convenience function for showing a popup with an error message
 function errorMessage(message) {
     alert("Error: " + message);
 }
 
+// This function takes the virtual barcode from the input box on the page and extracts the data within it.
+function parseVirtual() {
+    // Read the barcode and check that it's the correct length
+    let vCode = document.getElementById("virtual_barcode").value;
+    if (vCode.length != 54){
+        return errorMessage("invalid barcode length");
+    }
+
+    // Check which version the barcode is and invoke the correct algorithm to format the reference number with.
+    let version = vCode.slice(0, 1);
+    if (version == 4) {
+        refNo = refVer4(vCode.slice(28, 48));
+    } else if (version == 5) {
+        refNo = refVer5(vCode.slice(25, 48));
+    } else {
+        errorMessage("invalid barcode version");
+        return;
+    }
+
+    // Extract and format the rest of the data within the barcode: IBAN account, amount to be paid and due date.
+    let iban = formatIBAN(vCode.slice(1, 17));
+    let amount = parseInt(vCode.slice(17, 23), 10) + "," + vCode.slice(23, 25);
+    let dueDate = parseDate(vCode.slice(-6));
+
+    // Print the payment information on the webpage.
+    document.getElementById("iban").innerText = iban;
+    document.getElementById("amount").innerText = amount;
+    document.getElementById("reference").innerText = refNo;
+    document.getElementById("due_date").innerText = dueDate;
+
+    // Use JsBarcode to generate a a barcode in Code128-c format and display it on the web page.
+    let element = document.getElementById("barcode");
+    JsBarcode(element, vCode, {format: "CODE128C"});
+}
+
+// Function to parse the reference number from a version 4 barcode. Strips all zeroes from the beginning of the string
+// and arranges the remaining numbers in groups of five.
+function refVer4(rawRefNo){
+    rawRefNo = rawRefNo.replace(/^0+/, "");
+    refNo = "";
+    while (rawRefNo.length > 4) {
+        refNo = rawRefNo.slice(-5) + " " + refNo;
+        rawRefNo = rawRefNo.slice(0, -5);
+    }
+    refNo = rawRefNo + " " + refNo;
+    return refNo;
+}
+
+// Function to parse the reference number from a version 5 barcode. Starts with "RF" and the first two numbers in the
+// series, strips all zeroes until the next number, and arranges the remaining numbers in groups of four.
+function refVer5(rawRefNo) {
+    refNo = "RF" + rawRefNo.slice(0,2);
+    rawRefNo = rawRefNo.slice(2).replace(/^0+/, "");
+    while (rawRefNo.length > 3) {
+        refNo += " " + rawRefNo.slice(0, 4);
+        rawRefNo = rawRefNo.slice(4);
+    }
+    refNo += " " + rawRefNo;
+    return refNo;
+}
+
+// Function to convert a date from YYMMDD-form to DD.MM.YYYY
 function parseDate(rawDate) {
     if (rawDate == 0) {
         var date = "00.00.00";
@@ -14,61 +77,13 @@ function parseDate(rawDate) {
     return date;
 }
 
+// Function to group the digits in an IBAN account number so that they are easier to read
 function formatIBAN(rawNo) {
     formatted = rawNo.slice(0,2) + " " + rawNo.slice(2, 6) + " " + rawNo.slice(6, 10) + " " + rawNo.slice(10, 14) + " " + rawNo.slice(-2);
     return formatted;
 }
 
-function refVer4(rawRefNo){
-    rawRefNo = rawRefNo.replace(/^0+/, "");
-    refNo = "";
-    while (rawRefNo.length > 4) {
-        refNo = rawRefNo.slice(-5) + " " + refNo;
-        rawRefNo = rawRefNo.slice(0, -5);
-    }
-    refNo = rawRefNo + " " + refNo;
-    return refNo;
-}
-
-function refVer5(rawRefNo) {
-    refNo = "RF" + rawRefNo.slice(0,2);
-    rawRefNo = rawRefNo.slice(2).replace(/^0+/, "");
-    while (rawRefNo.length > 3) {
-        refNo += " " + rawRefNo.slice(0, 4);
-        rawRefNo = rawRefNo.slice(4);
-    }
-    refNo += " " + rawRefNo;
-    return refNo;
-}
-
-function parseVirtual() {
-    vCode = document.getElementById("virtual_barcode").value;
-    if (vCode.length != 54){
-        return errorMessage("invalid barcode length");
-    }
-
-    version = vCode.slice(0, 1);
-    if (version == 4) {
-        refNo = refVer4(vCode.slice(28, 48));
-    } else if (version == 5) {
-        refNo = refVer5(vCode.slice(25, 48));
-    } else {
-        errorMessage("invalid barcode version");
-        return;
-    }
-    iban = formatIBAN(vCode.slice(1, 17));
-    amount = parseInt(vCode.slice(17, 23), 10) + "," + vCode.slice(23, 25);
-    dueDate = parseDate(vCode.slice(-6));
-
-    document.getElementById("iban").innerText = iban;
-    document.getElementById("amount").innerText = amount;
-    document.getElementById("reference").innerText = refNo;
-    document.getElementById("due_date").innerText = dueDate;
-
-    var element = document.getElementById("barcode");
-    JsBarcode(element, vCode, {format: "CODE128C"});
-}
-
+// Function to switch between two texts in a given context. Used for the "hide/show"-button.
 function toggleText(element, textA, textB){
     if($(element).text() == textA) {
         element.text(textB);
@@ -77,20 +92,28 @@ function toggleText(element, textA, textB){
     }
 }
 
-window.onload = function(n) {
+// This function is executed when the page is loaded into the browser window.
+window.onload = function() {
+    // Create an event handler that makes the background of the text input grey when the element has focus and
+    // white when it doesn't.
     $("input:text").bind("focus blur", function() {
         $(this).toggleClass("bg_grey");
     });
 
-    document.getElementById("btn_decode").onclick = parseVirtual;
-
+    // Set a listener on the hide-button. Hide the information section and change the text on the button to "show" when
+    // the user presses the hide-button. Show the information section and change the text to "hide" when the user presses
+    // the button again.
     $("#btn_hide").click(function(){
         $("#information").slideToggle("fast", toggleText($(this), "Hide", "Show"));
     });
 
+    // Set an event handler that clicks the decode-button vhen enter is pressed in the text input field.
     $("#virtual_barcode").keypress(function (key) {
         if (key.which == 13) {
             $("#btn_decode").click();
         }
     });
+
+    // Execute the parseVirtual-function when the user presses the decode-button.
+    document.getElementById("btn_decode").onclick = parseVirtual;
 }
--- excercise2/exercise2.js
+++ excercise2/exercise2.js
@@ -1,7 +1,9 @@
+// Global variables: search history, the list of current markers on the map, and the map itself.
 var historyData = new Array();
 var markers = new Array();
 var map;
 
+// Create and render the dropdown menu with countries.
 function populateCountries() {
     var options = [ ["FI", "Finland"],
                     ["SE", "Sweden"],
@@ -13,6 +15,7 @@ function populateCountries() {
     });
 }
 
+// Get the approximate location of the user and call a function to create a map instance that is centered on said location.
 function makeMap(){
     navigator.geolocation.getCurrentPosition(drawMap);
 }
@@ -27,6 +30,7 @@ function drawMap(position) {
     });
 }
 
+// Function to set a marker with a specific name on a specific location on the map
 function setMarker(position, placeName){
     var marker = new google.maps.Marker({
         position: position,
@@ -37,49 +41,79 @@ function setMarker(position, placeName){
     map.setCenter(position);
 }
 
+// Function to remove all markers from the map and empty the list of markers.
 function clearMarkers() {
     for (let i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
     }
+    markers = [];
 }
 
+/* This is where the magic happens. ;) This function takes a country code and a zipcode as agruments and queries
+zipopotamus for the given data. The data the server returns is then used to:
+     - create a list of the location(s) associated with the given zipcode and
+     - place markers on those locations on the map (after any earlier markers are removed).
+*/
 function fetchData(countryCode, zipCode){
+    // Fetch data from server and execute a lambda callback-function:
     $.getJSON("http://api.zippopotam.us/" + countryCode + "/" + zipCode, function(data) {
+        // Delete old markers
         clearMarkers();
+
+        // Parse the data from the server
         let response = JSON.parse(JSON.stringify(data));
         let places = JSON.parse(JSON.stringify(response.places));
+
+        // Create a table with the results
         let result = '<table class="resultTable"><tr><td class="resultCol1">Place name</td><td class="resultCol2">Longitude</td><td class="resultCol3">Latitude</td></tr>';
+
+        // Iterate over all (if any) places that have the specified zip code:
         for (let i = 0; i < places.length; i++) {
+            // Create table rows with the names and coordinates of the places that have the specified zip code
             let position = new google.maps.LatLng(places[i].latitude, places[i].longitude);
             let placeName = places[i]["place name"];
             result += '<tr><td>'+ placeName + '</td><td>'+ places[i]["longitude"]+'</td><td>'+places[i]["latitude"]+'</td></tr>';
+            // Put markers on the map
             setMarker(position, placeName);
         }
         result += "</table>"
+
+        // Resize the results-section on the html-page
         let height = ((places.length + 1) *1.4) + "em";
         $("#box_results").css("height", height);
+
+        // Insert the table with results in the page
         $("#box_results").html(result);
     });
 }
 
+// Function executed when the user clicks on the search-button
 function searchClicked() {
     countryCode = $("#countries").val();
     zipCode = $("#zip").val();
 
+    // Update the search history
     historyData.unshift($("#countries option[value=" + countryCode + "]").text() + " " + zipCode + "<br>");
+    // If history is full, remove the oldest element
     if (historyData.length > 9) {
         historyData.pop();
     }
+    // Update the history shown on the html-page and
     $("#history").html(historyData);
+    // store the history on the users system.
     localStorage.setItem("history", historyData);
+    // Get the requested data.
     fetchData(countryCode, zipCode);
 }
 
+// Function to execute when the page is loaded into the browser window
 window.onload = function() {
     populateCountries();
+    // Load the saved search history from an earlier session
     historyData = localStorage.getItem("history").split(',');
     $("#history").html(historyData);
     $("#btn_search").click(searchClicked);
+    // When the zip-input is active and the user presses enter, fire off the search-button
     $("#zip").keypress(function (key) {
         if (key.which == 13) {
             $("#btn_search").click();
